ed136dcb44602043639dd08219987e6b29b23a43
adelinashahaj <adelinarucaj@icloud.com>
2023-05-26 12:54:53 +0200

    fatto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,24 @@
     <main>
 
         <div class="container">
-            @yield('content')
-            
+
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+
+            </div>
+           @endif
+         @yield('content')
         </div>
 
     </main>
